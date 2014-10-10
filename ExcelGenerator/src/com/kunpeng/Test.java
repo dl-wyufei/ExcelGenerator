618d3851024b37fa66ac3484e0d0b51c6fd2fc50
WangYufei <Yufei@ERDGT-016>
2014-10-10 20:22:18 +0800

    工程初始化

---
+++ ExcelGenerator/src/com/kunpeng/Test.java
@@ -0,0 +1,10 @@
+package com.kunpeng;
+
+public class Test {
+
+	public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
+		System.out.println("开始时间" + startTime);
+	}
+
+}
